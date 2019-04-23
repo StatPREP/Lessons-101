eccca51dfaf214ac1e1564d5d23e385fb0a4debe
Danny Kaplan <kaplan@macalester.edu>
2019-04-23 18:09:25 -0500

    Starting confidence interval topic.

---
+++ R/build.R
@@ -0,0 +1,7 @@
+# Create non-html versions of the documents
+
+blogdown::build_dir("static")
+
+# per the instructions in https://bookdown.org/yihui/blogdown/static-files.html
+# but it doesn't seem to work.
+
