bcd0d42ccb584236b8cd1757d92dae3821c7fd0d
Danny Kaplan <kaplan@macalester.edu>
2019-05-28 14:24:35 -0500

    Added PDF versions to site

--- R/lessons2word.R
+++ R/lessons2word.R
@@ -7,7 +7,7 @@ library(rmarkdown)
 
 source_files <- dir("content/lessons", pattern  = ".Rmd$", full.names = FALSE)
 
-#this_file <- source_files <- "what-is-a-confidence-interval.Rmd"
+#  for testing:source_files <- "summary-interval.Rmd"
 for (this_file  in source_files[]) {
   text <- readLines(paste0("content/lessons/",this_file))
   text <- gsub("/images/", "../../static/images/", text)
@@ -17,5 +17,9 @@ for (this_file  in source_files[]) {
          output_file = gsub(".Rmd$", ".docx", this_file), 
          output_format = "word_document", 
          output_dir = "static/word-versions")
+  render("content/lessons/foo.Rmd",
+         output_file = gsub("Rmd$", "pdf", this_file),
+         output_format = tint::tintPdf(),
+         output_dir = "static/pdf-versions")
 }
 file.remove("content/lessons/foo.Rmd")
--- R/topics2word.R
+++ R/topics2word.R
@@ -9,12 +9,16 @@ source_files <- dir("content/topics", pattern  = ".Rmd$", full.names = FALSE)
 
 for (this_file  in source_files[]) {
   text <- readLines(paste0("content/topics/",this_file))
-  text <- gsub("/images/", "../../static/images/", text)
+  text <- gsub("/images/", "../../static/images/", text, fixed = TRUE)
   text <- paste0(text, collapse = "\n")
   writeLines(text, con  = "content/topics/foo.Rmd")
   render("content/topics/foo.Rmd",  
          output_file = gsub(".Rmd$", ".docx", this_file), 
          output_format = "word_document", 
          output_dir = "static/word-versions")
+  render("content/topics/foo.Rmd",
+         output_file = gsub("Rmd$", "pdf", this_file),
+         output_format = tint::tintPdf(),
+         output_dir = "static/pdf-versions")
 }
 file.remove("content/topics/foo.Rmd")
